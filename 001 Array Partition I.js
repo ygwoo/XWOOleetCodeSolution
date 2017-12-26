a754a532fcc3ddb45992da2ab77a82d381e7cd06
yiguang woo <ygwoo@users.noreply.github.com>
2017-12-26 12:06:09 +0800

    rewrite
    1.delete HTML file;
    2.add JS file.
    3.Evn please choose Node

---
+++ 001 Array Partition I.js
@@ -0,0 +1,31 @@
+/**
+ * 001.Array Partition I-561
+ * 001.Given an array of 2n integers, your task is to group these integers into n pairs of integer, 
+ * say (a1, b1), (a2, b2), ..., (an, bn) which makes sum of min(ai, bi) for all i from 1 to n as large as possible.
+ * 
+ * Example 1:
+ * Input: [1,4,3,2]
+ * Output: 4
+ * 
+ * Note:
+ * n is a positive integer, which is in the range of [1, 10000].
+ * All the integers in the array will be in the range of [-10000, 10000].
+ */
+
+var outputArray = new Array();
+var inputArray = [1, 4, 3, 2];
+outputArray = inputArray.sort(sortNumber);
+
+function sortNumber(a, b) {
+	return a - b;
+}
+
+function showAnswer() {
+	var sum = 0;
+	for (var i = 0; i < outputArray.length; i += 2) {
+		sum += outputArray[i];
+	}
+	return sum;
+}
+console.log(outputArray);
+console.log(showAnswer());
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # XWOOleetCodeSolution
 LeetCode javascript answer
 
-1.这是一个JavaScript leetCode解题库
-2.不定时更新，可能不更新
-3.直接下载库中的***.html文件查看问题及答案
+1.这是一个JavaScript leetCode解题库;
+2.不定时更新，可能不更新;
+3.直接下载库中的[No].[LeetCode Problem Name].js 文件查看问题及答案。
